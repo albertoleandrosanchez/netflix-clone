ace46db8ad8b9f04ea0974f9465208cafb9fe95f
Beto <albertoleandrosanchez@hotmail.com>
2021-02-06 15:44:00 -0300

    arreglar la api de movie-trailers

--- src/Banner.js
+++ src/Banner.js
@@ -39,7 +39,7 @@ function Banner() {
 
         <div className="banner__contents">
             <h1 className="banner__title">
-                {movie?.title || movie?.name || movie?.name}
+                {movie?.title || movie?.name || movie?.original_name}
             </h1>   
             <div className="banner__buttons">
                 <button className="banner__button">Play</button>
--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import YouTube from 'react-youtube';
 import axios from "./axios";
-import "./Row.css"
+import "./Row.css";
+import movieTrailer from "movie-trailer";
 
 function Row({title, fetchUrl,isLargeRow}) {
 
-    let baseURL="https://image.tmdb.org/t/p/w500/"
+    let baseURL="https://image.tmdb.org/t/p/w500/";
     //seteo una lista vacia con useState
     const [movies, setMovies] = useState([]);
+    const [trailerUrl,setTrailerUrl] = useState('');
 
     useEffect(()=> {
         //si dejamos los [], corre cuando el Row cargue y no corras mas
@@ -19,7 +22,26 @@ function Row({title, fetchUrl,isLargeRow}) {
         fetchData();
     },[fetchUrl]) //ponemos como dependencia fetchUrl ya que queremos que cambie la info si esta es cambiada en algun momento.
 
-    
+    const opts = {
+        height:"390",
+        width: "100%",
+        playerVars: {
+            autoplay:1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if(trailerUrl){
+            setTrailerUrl('');
+        }
+        else{
+            movieTrailer(movie?.title || movie?.name || movie?.original_name ||movie?.original_title).then((url) =>{
+                console.log(url)
+                const urlParams = new URLSearchParams( new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch(err => console.log(movie));
+        }
+    }
 
     //el return es lo que va a mostrar siempre
     return (
@@ -29,12 +51,17 @@ function Row({title, fetchUrl,isLargeRow}) {
                 {movies.map(movie =>(
                     <img 
                     key={movie.id}
+                    onClick={() => handleClick(movie)}
                     className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
                     //&& hace que si se cumple la condicion AGREGA lo siguiente
                     src={`${baseURL}${isLargeRow? movie.poster_path: movie.backdrop_path}`} alt={movie.name}
                     />
+                    
                 ))}
             </div>
+            
+            
+           {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
         </div>
     )
 }
